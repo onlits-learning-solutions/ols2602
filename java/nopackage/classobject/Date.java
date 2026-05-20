1e302d52a4c16be0d64e522b8e1ac9a537f5992a
Manish Kumar Mishra <manishmishra.shg@gmail.com>
2026-05-20 08:21:26 +0530

    20260520

---
+++ java/nopackage/classobject/Date.java
@@ -0,0 +1,5 @@
+public class Date {
+    int dd;
+    int mm;
+    int yyyy;
+}
---
+++ java/nopackage/classobject/DateExample.java
@@ -0,0 +1,8 @@
+public class DateExample {
+    public static void main(String[] args) {
+        Date d;
+
+        System.out.print("Enter day: ");
+        // d.dd = 
+    }
+}
